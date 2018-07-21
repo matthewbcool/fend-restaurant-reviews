b9fe2233f537c03568b311019dca425895978e82
Matt Cool <matthewbcool@gmail.com>
2018-07-21 10:52:18 -0500

    service worker js update

--- service-worker.js
+++ service-worker.js
@@ -16,14 +16,3 @@
     });
   
   })();
-
-  //service-worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-}
