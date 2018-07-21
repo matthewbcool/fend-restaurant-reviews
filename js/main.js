5c56b14b098b25ee208631fa552332b70583bc95
Matt Cool <matthewbcool@gmail.com>
2018-07-21 09:55:03 -0500

    moved service worker reg to main js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-//service-worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-}
-
+  //service-worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
 
 let restaurants,
   neighborhoods,
