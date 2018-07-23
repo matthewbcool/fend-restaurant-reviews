b3d11aa4f881c61264765f0ab8909e6305e33e00
Matt Cool <matthewbcool@gmail.com>
2018-07-22 19:43:12 -0500

    updated cachename var

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-  
+    let cacheName = 'rw-cache-v1'
     self.addEventListener('install', function(event) {
       event.waitUntil(
         caches.open(cacheName).then(function(cache) {
