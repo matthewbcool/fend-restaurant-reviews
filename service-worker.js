98cd8cf7fab741bda609e12cb2e4fef2086e5a8c
Matt Cool <matthewbcool@gmail.com>
2018-07-27 14:57:58 -0500

    fetching edit on sw

--- service-worker.js
+++ service-worker.js
@@ -36,14 +36,12 @@
   
   
     self.addEventListener('fetch', function(event) {
-      /*
       event.respondWith(
         caches.match(event.request).then(function(response) {
           if (response) return response;
           return fetch(event.request);
         })
       );
-      */
      console.log('fetching')
     });
   
