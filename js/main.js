683bfd3758e4682bc8a62e6cb774b09b333f48de
Matt Cool <matthewbcool@gmail.com>
2018-07-27 11:15:36 -0500

    aria roles added and semantic fixes

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name + ' image';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
